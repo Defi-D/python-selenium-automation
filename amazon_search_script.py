ae4228fc97978ca1828e061b18f44c107ed2bf1d
Defi-D <darius_ellis@msn.com>
2022-04-22 23:03:02 -0700

    .\amazon_search_script.py

--- amazon_search_script.py
+++ amazon_search_script.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome(executable_path='\Users\Dtop2\Desktop\Git Lesson\Jobeasy\python-selenium-automation\chromedriver.exe)
+driver = webdriver.Chrome(executable_path='C:/Users\Dtop2\Desktop\Git Lesson\Jobeasy\python-selenium-automation\chromedriver.exe')
 driver.get('https://www.amazon.com/')
+driver.find_element(By.ID, 'twotabsearchtextbox').send_keys('coffee')
 
-driver find_element(By,ID,"twotabsearchtextbox').send_keys('coffee')
+driver.find_element(By.ID, 'nav-search-submit-button').click()
 
-driver find_element(By,ID,"nav-search-submit-button').click()
